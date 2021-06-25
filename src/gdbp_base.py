bf9dd8d59d6a3d28a4f29f33f6a5ee360bff2898
Remi Qirui FAN <fanqirui318@gmail.com>
2021-06-25 17:03:39 +0800

    initial commit

---
+++ src/gdbp_base.py
@@ -0,0 +1,313 @@
+from jax import numpy as jnp, random, jit, value_and_grad
+import flax
+from commplax import util, comm, cxopt, op
+from commplax.module import core, layer
+import numpy as np
+from functools import partial
+from collections import namedtuple
+from tqdm import tqdm
+from typing import Any, Optional, Union
+from src import dataloader as dl
+
+
+Model = namedtuple('Model', 'model initvar overlaps')
+Array = Any
+Dict = Union[dict, flax.core.FrozenDict]
+
+
+def make_base_model(steps: int = 3,
+                    dtaps: int = 261,
+                    ntaps: int = 41,
+                    rtaps: int = 61,
+                    init_fn: tuple = (core.delta, core.gauss),
+                    w0 = 0.,
+                    mode: str = 'train'):
+    '''
+    make base model that derives DBP, FDBP, EDBP, GDBP depending on
+    specific initialization method and trainable parameters defined
+    by trainer.
+
+    Args:
+        steps: GDBP steps/layers
+        dtaps: D-filter length
+        ntaps: N-filter length
+        rtaps: R-filter length
+        init_fn: a tuple contains a pair of initializer for D-filter and N-filter
+        mode: 'train' or 'test'
+
+    Returns:
+        A layer object
+    '''
+
+    d_init, n_init = init_fn
+
+    if mode == 'train':
+        # configure mimo to its training mode
+        mimo_train = True
+    elif mode == 'test':
+        # mimo operates at training mode for the first 200000 symbols,
+        # then switches to tracking mode afterwards
+        mimo_train = cxopt.piecewise_constant([200000], [True, False])
+    else:
+        raise ValueError('invalid mode %s' % mode)
+
+    base = layer.Serial(
+        layer.FDBP(steps=steps,
+                   dtaps=dtaps,
+                   ntaps=ntaps,
+                   d_init=d_init,
+                   n_init=n_init),
+        layer.BatchPowerNorm(mode=mode),
+        layer.MIMOFOEAf(name='FOEAf',
+                        w0=w0,
+                        train=mimo_train,
+                        preslicer=core.conv1d_slicer(rtaps),
+                        foekwargs={}),
+        layer.vmap(layer.Conv1d)(name='RConv', taps=rtaps),  # vectorize column-wise Conv1D
+        layer.MIMOAF(train=mimo_train))  # adaptive MIMO layer
+    return base
+
+
+def fdbp_init(a: dict,
+              xi: float = 1.1,
+              steps: Optional[int] = None):
+    '''
+        initializer for the base model
+
+        Args:
+            xi: NLC scaling factor
+            steps: GDBP steps, used to calculate the theoretical profiles of D- and N-filters
+
+        Returns:
+            a pair of functions to initialize D- and N-filters
+    '''
+
+    def d_init(key, shape, dtype=jnp.complex64):
+        dtaps = shape[0]
+        d0, _ = comm.dbp_params(
+            a['samplerate'],
+            a['distance'] / a['spans'],
+            a['spans'],
+            dtaps,
+            a['lpdbm'] - 3,  # rescale as input power which has been norm to 2 in dataloader
+            virtual_spans=steps)
+        return d0[0, :, 0]
+
+    def n_init(key, shape, dtype=jnp.float32):
+        dtaps = shape[0]
+        _, n0 = comm.dbp_params(
+            a['samplerate'],
+            a['distance'] / a['spans'],
+            a['spans'],
+            dtaps,
+            a['lpdbm'] - 3,  # rescale
+            virtual_spans=steps)
+
+        return xi * n0[0, 0, 0] * core.gauss(key, shape, dtype)
+
+    return d_init, n_init
+
+
+def model_init(data: dl.Dataset,
+               base_conf: dict,
+               sparams_flatkeys: list,
+               n_symbols: int = 2000,
+               sps : int = 2):
+    ''' initialize model from base template, generating CDC, DBP, EDBP, FDBP, GDBP
+    depending on given N-filter length and trainable parameters
+
+    Args:
+        data:
+        base_conf: a dict of kwargs to make base model, see `make_base_model`
+        sparams_flatkeys: a list of keys contains the static(nontrainable) parameters.
+            For example, assume base model has parameters represented as nested dict
+            {'color': 'red', 'size': {'width': 1, 'height': 2}}, its flatten layout is dict
+             {('color',): 'red', ('size', 'width',): 1, ('size', 'height'): 2}, a sparams_flatkeys
+             of [('color',): ('size', 'width',)] means 'color' and 'size/width' parameters are static.
+            regexp key is supportted.
+        n_symbols: number of symbols used to initialize model, use the minimal value greater than channel
+            memory
+        sps: sample per symbol. Only integer sps is supported now.
+
+    Returns:
+        a initialized model wrapped by a namedtuple
+    '''
+
+    model = make_base_model(**base_conf, w0=data.w0)
+    y0 = data.y[:n_symbols * sps]
+    rng0 = random.PRNGKey(0)
+    z0, v0 = model.init(rng0, core.Signal(y0))
+    ol = z0.t.start - z0.t.stop
+    sparams, params = util.dict_split(v0['params'], sparams_flatkeys)
+    state = v0['af_state']
+    aux = v0['aux_inputs']
+    const = v0['const']
+    return Model(model, (params, state, aux, const, sparams), ol)
+
+
+def loss_fn(model: layer.Layer,
+            params: Dict,
+            state: Dict,
+            y: Array,
+            x: Array,
+            aux: Dict,
+            const: Dict,
+            sparams: Dict):
+    ''' loss function
+
+        Args:
+            model: model returned by `model_init`
+            params: trainable parameters
+            state: model state
+            y: transmitted waveforms
+            x: aligned sent symbols
+            aux: auxiliary input
+            const: contants (internal info generated by model)
+            sparams: static parameters
+
+        Return:
+            loss, updated model state
+    '''
+
+    params = util.dict_merge(params, sparams)
+    z, updated_state = model.apply(
+        {
+            'params': params,
+            'aux_inputs': aux,
+            'const': const,
+            **state
+        }, core.Signal(y))
+    loss = jnp.mean(jnp.abs(z.val - x[z.t.start:z.t.stop])**2)
+    return loss, updated_state
+
+
+@partial(jit, backend='cpu', static_argnums=(0, 1))
+def update_step(model: layer.Layer,
+                opt: cxopt.Optimizer,
+                i: int,
+                opt_state: tuple,
+                model_state: Dict,
+                y: Array,
+                x: Array,
+                aux: Dict,
+                const: Dict,
+                sparams: Dict):
+    ''' single backprop step
+
+        Args:
+            model: model returned by `model_init`
+            opt: optimizer
+            i: iteration counter
+            opt_state: optimizer state
+            model_state: model state
+            y: transmitted waveforms
+            x: aligned sent symbols
+            aux: auxiliary input
+            const: contants (internal info generated by model)
+            sparams: static parameters
+
+        Return:
+            loss, updated model state
+    '''
+
+    params = opt.params_fn(opt_state)
+    (loss, model_state), grads = value_and_grad(
+        loss_fn, argnums=1, has_aux=True)(model, params, model_state, y, x,
+                                          aux, const, sparams)
+    opt_state = opt.update_fn(i, grads, opt_state)
+    return loss, opt_state, model_state
+
+
+def get_train_batch(ds: dl.Dataset,
+                    batchsize: int,
+                    overlaps: int,
+                    sps: int = 2):
+    ''' generate overlapped batch input for training
+
+        Args:
+            ds: dataset
+            batchsize: batch size in symbol unit
+            overlaps: overlaps in symbol unit
+            sps: samples per symbol
+
+        Returns:
+            number of symbols,
+            zipped batched triplet input: (recv, sent, fomul)
+    '''
+
+    flen = batchsize + overlaps
+    fstep = batchsize
+    ds_y = op.frame_gen(ds.y, flen * sps, fstep * sps)
+    ds_x = op.frame_gen(ds.x, flen, fstep)
+    n_batches = op.frame_shape(ds.x.shape, flen, fstep)[0]
+    return n_batches, zip(ds_y, ds_x)
+
+
+def train(model: Model,
+          data: dl.Dataset,
+          batch_size: int = 500,
+          n_iter=None,
+          opt: cxopt.Optimizer = cxopt.adam(cxopt.piecewise_constant([500, 1000], [1e-4, 1e-5, 1e-6]))):
+    ''' training process (1 epoch)
+
+        Args:
+            model: Model namedtuple return by `model_init`
+            data: dataset
+            batch_size: batch size
+            opt: optimizer
+
+        Returns:
+            yield loss, trained parameters, model state
+    '''
+
+    params, model_state, aux, const, sparams = model.initvar
+    opt_state = opt.init_fn(params)
+
+    n_batch, batch_gen = get_train_batch(data, batch_size, model.overlaps)
+    n_iter = n_batch if n_iter is None else min(n_iter, n_batch)
+
+    for i, (y, x) in tqdm(enumerate(batch_gen),
+                             total=n_iter, desc='training', leave=False):
+        if i >= n_iter: break
+        aux = core.dict_replace(aux, {'truth': x})
+        loss, opt_state, model_state = update_step(model.model, opt, i, opt_state,
+                                                   model_state, y, x, aux,
+                                                   const, sparams)
+        yield loss, opt.params_fn(opt_state), model_state
+
+
+def test(model: Model,
+         params: Dict,
+         data: dl.Dataset,
+         eval_range: tuple=(300000, -20000),
+         metric_fn=comm.qamqot):
+    ''' testing, a simple forward pass
+
+        Args:
+            model: Model namedtuple return by `model_init`
+        data: dataset
+        eval_range: interval which QoT is evaluated in, assure proper eval of steady-state performance
+        metric_fn: matric function, comm.snrstat for global & local SNR performance, comm.qamqot for
+            BER, Q, SER and more metrics.
+
+        Returns:
+            evaluated matrics and equalized symbols
+    '''
+
+    state, aux, const, sparams = model.initvar[1:]
+    aux = core.dict_replace(aux, {'truth': data.x})
+
+    z, _ = jit(model.model.apply,
+               backend='cpu')({
+                   'params': util.dict_merge(params, sparams),
+                   'aux_inputs': aux,
+                   'const': const,
+                   **state
+               }, core.Signal(data.y))
+    metric = metric_fn(z.val,
+                       data.x[z.t.start:z.t.stop],
+                       scale=np.sqrt(10),
+                       eval_range=eval_range)
+    return metric, z
+
+
---
+++ src/plot.py
@@ -0,0 +1,82 @@
+import matplotlib.pyplot as plt
+from matplotlib.ticker import FormatStrFormatter
+import numpy as np
+from commplax import plot as cplt, comm
+
+
+def lp_vs_q(lp, q, ax=None, **kwargs):
+    if ax is None:
+        plt.figure()
+        ax = plt.gca()
+    ax.plot(lp, q, '-o', **kwargs)
+    ax.legend()
+    ax.set_xlabel('Launched Power (dBm)')
+    ax.set_ylabel('Q-facotr (dB)')
+
+
+def wireframe_cmap(ax, X, Y, Z, cmap=plt.cm.viridis, offset=[0, 0], label=""):
+    norm = plt.Normalize(Z.min() + offset[0], Z.max() + offset[1])
+    colors = cmap(norm(Z))
+    surf = ax.plot_surface(X,
+                           Y,
+                           Z,
+                           facecolors=colors,
+                           shade=False,
+                           label=label)
+    surf._facecolors2d = surf._facecolor3d
+    surf._edgecolors2d = surf._edgecolor3d
+    surf.set_facecolor((0, 0, 0, 0))
+
+
+def loss(loss, ax=None, label=None, alpha=0.4):
+    if ax is None:
+        plt.figure()
+        ax = plt.gca()
+    loss_mean = np.convolve(loss, np.ones(100) / 100, mode='same')
+    p = ax.plot(loss[:], alpha=alpha, label=label)
+    ax.plot(loss_mean[:-50], color=p[0].get_color())
+    ax.set_xlabel('iteration')
+    ax.set_ylabel('MSE')
+    ax.legend()
+
+
+def gdbp_params(params, vertical=False, sr=72, bw=60, dpi=100):
+    if vertical:
+        fig, axs = plt.subplots(3, 1, figsize=(4, 8), dpi=dpi)
+    else:
+        fig, axs = plt.subplots(1, 3, figsize=(10, 2.5), dpi=dpi)
+    param_D = np.stack(
+        [params[-5]['fdbp_0']['DConv_%d' % i]['kernel'] for i in range(3)])
+    param_N = np.stack(
+        [params[-5]['fdbp_0']['NConv_%d' % i]['kernel'] for i in range(3)])
+
+    ax = axs[0]
+    cplt.desc_filter(*comm.firfreqz(param_D[:, :, 0], sr=sr, bw=bw),
+                     ax=ax,
+                     legend=['Step 1', 'Step 2', 'Step 3'],
+                     colors=plt.cm.RdPu_r(np.linspace(0, 0.8, len(param_D))))
+    ax.set_xlabel('freq. (GHz)')
+    ax.yaxis.set_major_formatter(FormatStrFormatter('%.2f'))
+
+    ax = axs[1]
+    cplt.desc_filter(*comm.firfreqz(param_N[:, :, 0, 0], sr=sr, bw=bw),
+                     ax=ax,
+                     legend=['Step 1', 'Step 2', 'Step 3'],
+                     colors=plt.cm.RdPu_r(np.linspace(0, 0.8, len(param_D))),
+                     Hermitian=True)
+    ax.set_xlabel('freq. (GHz)')
+    ax.set_ylim([-0.05, 0.25])
+    ax.yaxis.set_major_formatter(FormatStrFormatter('%.2f'))
+
+    ax = axs[2]
+    cplt.desc_filter(*comm.firfreqz(params[-5]['RConv']['kernel'][:, 0],
+                                    sr=sr,
+                                    bw=bw),
+                     ax=ax)
+    ax.legend().set_visible(False)
+    ax.set_xlabel('freq. (GHz)')
+    ax.yaxis.set_major_formatter(FormatStrFormatter('%.2f'))
+
+    fig.tight_layout()
+
+
